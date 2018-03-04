c85d97f42903f834d27703e6375bbb8b9ce4dd16
Roman-Liubinets <roman.liubinets@gmail.com>
2018-03-04 21:03:29 +0200

    ss

--- public/script/script.js
+++ public/script/script.js
@@ -1,1 +1,38 @@
-alert("hello world")
+const app = angular.module('app', ['ngDialog']);
+
+//Котроллер
+app.controller("myCtrl", function($scope, $http, ngDialog) {});
+
+//Директиви
+
+app.directive("headerBlock", function() {
+    return {
+        replace: true,
+        templateUrl: "template/header.html",
+        controller: function($scope, $http, ngDialog) {}
+    }
+});
+
+app.directive("bodyBlock", function() {
+    return {
+        replace: true,
+        templateUrl: "template/body.html",
+        controller: function($scope, $http, ngDialog) {}
+    }
+});
+
+app.directive("footerBlock", function() {
+    return {
+        replace: true,
+        templateUrl: "template/footer.html",
+        controller: function($scope, $http, ngDialog) {}
+    }
+});
+
+app.directive("contentBlock", function() {
+    return {
+        replace: true,
+        templateUrl: "template/pages/Content.html",
+        controller: function($scope, $http, ngDialog) {}
+    }
+});
