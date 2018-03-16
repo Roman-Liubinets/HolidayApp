c47229ad1fff0418699466f135fa68871921caaa
Roman-Liubinets <roman.liubinets@gmail.com>
2018-03-16 15:08:00 +0200

    add in script js object with our date then repeat in html

--- public/script/script.js
+++ public/script/script.js
@@ -1,38 +1,64 @@
 const app = angular.module('app', ['ngDialog']);
 
 //Котроллер
-app.controller("myCtrl", function($scope, $http, ngDialog) {});
+app.controller("myCtrl", function ($scope, $http, ngDialog) {});
 
 //Директиви
 
-app.directive("headerBlock", function() {
+app.directive("headerBlock", function () {
     return {
         replace: true,
         templateUrl: "template/header.html",
-        controller: function($scope, $http, ngDialog) {}
+        controller: function ($scope, $http, ngDialog) {}
     }
 });
 
-app.directive("bodyBlock", function() {
+app.directive("bodyBlock", function () {
     return {
         replace: true,
         templateUrl: "template/body.html",
-        controller: function($scope, $http, ngDialog) {}
+        controller: function ($scope, $http, ngDialog) {}
     }
 });
 
-app.directive("footerBlock", function() {
+app.directive("footerBlock", function () {
     return {
         replace: true,
         templateUrl: "template/footer.html",
-        controller: function($scope, $http, ngDialog) {}
+        controller: function ($scope, $http, ngDialog) {}
     }
 });
 
-app.directive("contentBlock", function() {
+app.directive("contentBlock", function () {
     return {
         replace: true,
         templateUrl: "template/pages/Content.html",
-        controller: function($scope, $http, ngDialog) {}
+        controller: function ($scope, $http, ngDialog) {
+            $scope.tableWeatherContainer = [{
+                name: "Tirana",
+                temperature: "-23",
+                condition: "Smooth"
+            }, {
+                name: "Andorra la Vella",
+                temperature: "-13",
+                condition: "Smooth"
+            }, {
+                name: "Yerevan",
+                temperature: "-25",
+                condition: "Smooth"
+            }, {
+                name: "Vienna",
+                temperature: "-3",
+                condition: "Smooth"
+            }, {
+                name: "Baku",
+                temperature: "-18",
+                condition: "Smooth"
+            }, {
+                name: "Minsk",
+                temperature: "-14",
+                condition: "Smooth"
+            }]
+        }
     }
 });
